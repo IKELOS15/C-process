6d76b35fb88ba9f388fe433e6024ac9535f6175e
IKELOS15 <96808740+IKELOS15@users.noreply.github.com>
2024-11-23 15:45:17 +0900

    Add files via upload

---
+++ 12주차/레포트/main (4).c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <time.h>
+#include "report.h"
+#include "wday.h"
+int main(int argc, char* argv[])
+{
+    week2(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+}
---
+++ 12주차/레포트/wday.h
@@ -0,0 +1,25 @@
+#ifndef _WDAY_H
+#define _WDAY_H
+void wday(){
+    time_t current;
+    struct tm* timeinfo;
+    
+    int year, month, day;
+    char* WEEK[] = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
+    
+    printf("Enter YYYY MM DD : ");
+    scanf("%d %d %d", &year, &month, &day);
+    
+    current = time(NULL);
+    timeinfo = localtime(&current);
+    
+    timeinfo->tm_year = year - 1900;
+    timeinfo->tm_mon = month -1;
+    timeinfo->tm_mday = day;
+    
+    mktime(timeinfo);
+    
+    printf("%d %d %d -> %s\n", year, month, day, WEEK[timeinfo->tm_wday]);
+}
+
+#endif
