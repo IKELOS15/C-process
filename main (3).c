cb9c09e4d18e88f34db3a60a5337f9a83d55675e
IKELOS15 <96808740+IKELOS15@users.noreply.github.com>
2024-11-23 15:41:48 +0900

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include "report.h"
+
+int main(int argc, char* argv[])
+{
+    
+    time_t current;
+    struct tm* timeinfo;
+    
+    int year, month, day;
+    char* WEEK[] = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
+    
+    printf("Enter YYYY MM DD : ");
+    scanf("%d %d %d", &year, &month, &day);
+    
+    current = time(NULL);
+    timeinfo = localtime(&current);
+    
+    timeinfo->tm_year = year - 1900;
+    timeinfo->tm_mon = month -1;
+    timeinfo->tm_mday = day;
+    
+    mktime(timeinfo);
+    
+    printf("%d %d %d -> %s\n", year, month, day, WEEK[timeinfo->tm_wday]);
+    
+    return 0;
+}
